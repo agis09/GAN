502db6bd740667ea8d3a4a2f9cd2d51b39f69bfe
agis09 <33488378+agis09@users.noreply.github.com>
2018-08-16 16:28:11 +0900

    Update resize.py

--- illust_make/resize.py
+++ illust_make/resize.py
@@ -2,23 +2,30 @@ import os
 import glob
 from PIL import Image,ImageFilter,ImageDraw,ImageOps
 from tqdm import tqdm
+import warnings
 
-files_jpeg = glob.glob('./solo+standing+1girl/*.jpeg')
-files_png = glob.glob('./solo+standing+1girl/*.png')
+files_jpeg = glob.glob('/home/dmitri/PycharmProjects/GAN/illust_make/solo+standing+1girl/*.jpeg')
+files_png = glob.glob('/home/dmitri/PycharmProjects/GAN/illust_make/solo+standing+1girl/*.png')
 files = files_jpeg+files_png
-w=256
-h=256
+w=128
+h=128
 
 
 for f in tqdm(files):
-    img = Image.open(f)
-    img.thumbnail((256,256),Image.ANTIALIAS)
+    warnings.filterwarnings('error')
+    try:
+        img = Image.open(f)
+        img.thumbnail((128, 128), Image.ANTIALIAS)
 
-    bg = Image.new("RGBA", [w, h], (0, 0, 0, 0))
-    bg.paste(img,((w-img.size[0])//2,(h-img.size[1])//2))
-    f_name, ext = os.path.splitext(os.path.basename(f))
-    # print(f_name)
-    bg.save('./resized/'+f_name+'.png')
-    bg_r = ImageOps.mirror(bg)
-    bg_r.save('./resized/'+f_name+'_r.png')
+        bg = Image.new("RGBA", [w, h], (0, 0, 0, 0))
+        bg.paste(img, ((w - img.size[0]) // 2, (h - img.size[1]) // 2))
+        f_name, ext = os.path.splitext(os.path.basename(f))
+        # print(f_name)
+        bg.save('./dcgan/resized/solo+standing+1girl/' + f_name + '.png')
+        bg_r = ImageOps.mirror(bg)
+        bg_r.save('./dcgan/resized/solo+standing+1girl/' + f_name + '_r.png')
+    except Warning:
+        print("warning raised")
+        continue
+
 
