60ae5eeaea45a9f0747ea94612d59790949d304c
agis09 <33488378+agis09@users.noreply.github.com>
2018-09-17 01:37:53 +0900

    Update scraping_downloader.py

--- scraping_downloader.py
+++ scraping_downloader.py
@@ -1,143 +1,49 @@
-#!/usr/bin/env python3
-
-import requests, os, bs4
-
+import requests, os
+from bs4 import BeautifulSoup
 import cv2
-import os
 from glob import glob
 from tqdm import tqdm
-from bs4 import BeautifulSoup as bs
-from queue import Queue
-import threading
-import requests
-import json
-import time
-import os
 
 
-def download():
-    while not q.empty():
-        info = q.get()
-        name = info[11:]
-        url = "http://e-shuushuu.net/images/" + str(info)
-        path = "./e-shuushuudownloaded/" + info[:10] + "/"
+out_face_path = 'E:\\pict_make\\face_add\\'
+xml_path = "E:\\pict_make\\lbpcascade_animeface.xml"
 
-        if not os.path.exists(path):
-            os.makedirs(path)
+save_dir = 'E:\\pict_make\\tmp'
 
-        headers = {
-            'User-agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0'
-        }
-
-        r = requests.get(url, headers=headers, stream=True)
-
-        if r:
-            with open(str(path + name), "wb") as f:
-                for chunk in r.iter_content(chunk_size=1024):
-                    if chunk:
-                        f.write(chunk)
-
-        q.task_done()
-
-        print("Downloaded {}".format(name))
+target_url = 'http://e-shuushuu.net'
+page_tag = '/?page='
 
 
-def main():
-    datafile, cont, opening, recent = json.loads(open("data.json", "r").read()), False, False, 0
-    try:
-        data = int(datafile["last"])
+def download_image(url, directory):
+    '''Save an image from a URL to a new directory.'''
+    image = requests.get(url)
+    filetype = image.headers['content-type'].split('/')[-1]
+    # print(url.split('/'))
+    name = directory + "/" + url.split("/")[-1] + "." + filetype
 
-    except:
-        data = 0
-        opening = True
+    file = open(name, 'wb')
+    file.write(image.content)
+    file.close()
 
-    if data > 0 and not opening:
-        x = input("Would you like to continue downloading from where you left off (Y/n)? ")
-        if x.lower() not in ['y', 'yes', 'n', 'no']:
-            print("Error, invalid option {}. Please try again.".format(x))
-            return
 
-        if x.lower() in ['y', 'yes']:
-            cont = True
+while True:
+    page_count = 1
+    url =  target_url + page_tag + str(page_count)
+    req = requests.get(url)
+    print("\n")
+    print("loading..."+url)
+    print(req)
 
-        else:
-            data = 0
+    if req == None:
+        print("Finished! Downloaded " + str(page_count) + " images!")
+        break
 
-    pages = input("How many pages of images would you like to download? ")
+    soup = BeautifulSoup(req.text, 'lxml')
 
-    threads = input("Amount of threads to download with (type \"default\" for default number)? ")
+    for a in soup.find_all("a", class_="thumb_image"):
+        img_url = target_url + a.get('href')
+        print('saving...' + img_url)
+        download_image(img_url, save_dir)
 
-    if not pages.isnumeric():
-        print("Error: {} is not an integer")
-        return
 
-    else:
-        pages = int(pages)
 
-    if threads.lower() == "default":
-        threads = "4"
-
-    if not threads.isnumeric():
-        print("Error: {} is not an integer".format(threads))
-        return
-
-    else:
-        threads = int(threads)
-
-    if pages <= 0:
-        print("Error, bad page amount")
-        return
-
-    global q, debug
-    q = Queue()
-    debug = []
-
-    start = time.time()
-
-    for i in range(pages):
-        try:
-            page_no = int(i) + 1 + int(data)
-            page = requests.get("http://e-shuushuu.net/?page=" + str(page_no))
-            if page:
-                soup = bs(page.content, "html.parser")
-
-                main = soup.find("div", {"id": "page"})
-                content = main.find("div", {"id": "content"})
-                links = []
-                for x in content.findAll("div", {"class": "image_thread display"}):
-                    for y in x.findAll("div", {"class": "image_block"}):
-                        z = y.find("div", {"class": "thumb"})
-                        a = z.find("a", {"class": "thumb_image"})
-                        links.append(a["href"].replace("/images/", "").strip())
-
-                for blah in links:
-                    q.put(blah)
-                    debug.append(blah)
-
-                print("Retrieved images from page {}".format(page_no))
-
-            else:
-                print("Error getting page")
-                continue
-
-        except:
-            print("Error, something went wrong when finding images")
-
-        recent = int(page_no)
-
-    for i in range(threads):
-        t = threading.Thread(target=download)
-        t.daemon = True
-        t.start()
-        t.join()
-
-    print("Downloaded all images")
-
-    f = open("data.json", "w")
-    datafile["last"] = int(recent) + 1
-    f.write(json.dumps(datafile))
-    f.close()
-
-    stop = time.time()
-
-    print("Total elapsed time: {} seconds".format(int(stop) - int(start)))
